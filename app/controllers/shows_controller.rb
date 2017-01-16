2db11a85703352927fd7f644e7ba1a7ff32a112a
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-16 17:31:20 +0000

    adding create route

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,4 +4,13 @@ class ShowsController < ApplicationController
     render :json => "test"
   end
 
+  def create
+    @show = Show.create( show_params )
+    render :json => @show
+  end
+
+  def show_params
+    params.require(:show).permit(:title, :series, :description, :image, :programmeId)
+  end
+
 end
